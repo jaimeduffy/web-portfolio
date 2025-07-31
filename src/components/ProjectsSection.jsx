1e9413fe0c862734ee54a839d418213da12234c2
jaimeduffy <jaime.duffypanes@gmail.com>
2025-07-31 20:15:44 +0200

    footer

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
 const projects = [
     {
@@ -25,8 +26,38 @@ export const ProjectsSection = () => {
                         <div className="h-48 overflow-hidden">
                             <img src= {project.image} alt= {project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
                         </div>
+
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">{tag}</span>
+                                ))}
+                            </div>
+                        
+
+                            <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                            <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    {/* 
+                                    <a href={porject.demoUrl} target="_blank" className="text-foreground/80 hover:textprimary transition-colors duration-300">
+                                        <ExternalLink size={20}/>
+                                    </a>
+                                    */}
+                                    <a href={project.githubUrl} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                        <Github size={20}/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 ))}
+            </div>
+
+            <div className="text-center mt-12">
+                <a className="cosmic-button w-fit flex items-center mx-auto gap-2" target="_blank" href="https://github.com/jaimeduffy">
+                    Check My Github <ArrowRight size={16} />
+                </a>
             </div>
         </div>
     </section>;
